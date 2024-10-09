6f100ed49a5ed3ca11f106cd606b35966f73cd48
ROUSSARAZ24 <frazuajev@gmail.com>
2024-10-09 09:31:49 -0500

    carpeta

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuToggleButton = document.getElementById("menu-toggle-button")
-
-const navMenu = document.getElementById("nav-menu")
-
-
-menuToggleButton.addEventListener("click", function() {
-    navMenu.classList.toggle("visible")
-})
